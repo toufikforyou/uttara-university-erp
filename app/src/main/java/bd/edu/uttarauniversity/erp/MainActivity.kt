2e988538da6ef01d0b1a9e0be850e2e9e471eae4
MH TOUFIK <toufikforyou@gmail.com>
2025-07-30 12:44:37 +0600

    Add loading indicator to WebView during page load

--- app/src/main/java/bd/edu/uttarauniversity/erp/MainActivity.kt
+++ app/src/main/java/bd/edu/uttarauniversity/erp/MainActivity.kt
@@ -1,6 +1,7 @@
 package bd.edu.uttarauniversity.erp
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.ViewGroup
 import android.webkit.WebSettings
@@ -9,10 +10,17 @@ import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import bd.edu.uttarauniversity.erp.ui.theme.UUTheme
@@ -39,20 +47,36 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WebViewExample(modifier: Modifier) {
     val mUrl = "https://erp.uttarauniversity.edu.bd/"
+    var isLoading by remember { mutableStateOf(true) }
 
-    AndroidView(modifier = modifier, factory = {
-        WebView(it).apply {
-            settings.javaScriptEnabled = true
-            settings.javaScriptCanOpenWindowsAutomatically = true
-            settings.mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
-            settings.domStorageEnabled = true
-            settings.cacheMode = WebSettings.LOAD_DEFAULT
+    Box(modifier = modifier) {
+        AndroidView(modifier = Modifier.fillMaxSize(), factory = {
+            WebView(it).apply {
+                settings.javaScriptEnabled = true
+                settings.javaScriptCanOpenWindowsAutomatically = true
+                settings.mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
+                settings.domStorageEnabled = true
+                settings.cacheMode = WebSettings.LOAD_DEFAULT
 
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            webViewClient = WebViewClient()
-            loadUrl(mUrl)
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = object : WebViewClient() {
+                    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                        super.onPageStarted(view, url, favicon)
+                        isLoading = true
+                    }
+
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                        isLoading = false
+                    }
+                }
+                loadUrl(mUrl)
+            }
+        })
+        if (isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
-    })
+    }
 }
